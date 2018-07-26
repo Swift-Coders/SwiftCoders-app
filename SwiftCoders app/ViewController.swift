55940fe2a1b7c4154378332c12587b6a08cc060c
Yariv Nissim <yariv@tradesy.com>
2018-07-25 19:30:23 -0700

    Initial Commit

---
+++ SwiftCoders app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCoders app
+//
+//  Created by Yariv Nissim on 7/25/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
